9b02ec0b7d10b586d4ff9db7a2fa7f50c80fd8e1
Tyson Key <tyson.key@gmail.com>
2021-02-05 01:25:14 +0100

    We don't need the wrapper

--- Build/evf-2.0/decode_vos_file.d
+++ Build/evf-2.0/decode_vos_file.d
@@ -1,2 +1,2 @@
 Build/evf-2.0/decode_vos_file.o: evf-2.0/decode_vos_file.c \
- evf-2.0/vostypes.h evf-2.0/u.encode_vos_file.h
+ evf-2.0/vostypes.h evf-2.0/char_varying.h evf-2.0/u.encode_vos_file.h
--- evf-2.0/vostypes.h
+++ evf-2.0/vostypes.h
@@ -1,11 +1,12 @@
 /*
  *   stdtypes.h     Standard Types used for VOS applications.
  */
-//#include <char_varying.h>
-#ifndef char_varying
-#define char_varying(maxlen_) \
-	  struct { short len ; char str [(maxlen_)]; }
-#endif
+#include "char_varying.h"
+//#ifndef char_varying
+//#define char_varying(maxlen_) \
+//	  struct { short len ; char str [(maxlen_)]; }
+//#endif
+#define strcpy_vstr_nstr strcpy
 
 typedef char_varying(32)      object_t;
 typedef char_varying(66)      device_t;
