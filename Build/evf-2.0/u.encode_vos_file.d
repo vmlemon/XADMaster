046059d6ef2746308b25e237184094a00d38d0a0
Tyson Key <tyson.key@gmail.com>
2021-02-05 01:26:18 +0100

    Re-enable the standalone

--- Build/evf-2.0/u.encode_vos_file.d
+++ Build/evf-2.0/u.encode_vos_file.d
@@ -1,3 +1,3 @@
 Build/evf-2.0/u.encode_vos_file.o: evf-2.0/u.encode_vos_file.c \
  evf-2.0/system_io_constants.incl.c evf-2.0/file_status_info.incl.c \
- evf-2.0/vostypes.h evf-2.0/u.encode_vos_file.h
+ evf-2.0/vostypes.h evf-2.0/char_varying.h evf-2.0/u.encode_vos_file.h
